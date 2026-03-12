fdc3f9803db42115883f3b4b30a09f6ccccaa944
fernandatperez <fernanda.tperez@outlook.com>
2026-03-11 22:34:50 -0300

    +book search options for filter in /home

---
+++ src/main/kotlin/model/BookSearchCriteria.kt
@@ -0,0 +1,16 @@
+package model
+
+import java.time.LocalDate
+
+// esto no se lo vemos que opinan
+data class BookSearchCriteria (
+    val gender: Gender? = null,
+    val pagesRangeFrom: Int? = null,
+    val pagesRangeTo: Int? = null,
+    val title: String? = null,
+    val ISBN: String? = null,
+    val ownBy: User? = null,
+    val from: LocalDate = LocalDate.now(),
+    val to: LocalDate = LocalDate.now(),
+)
+
--- src/main/kotlin/repository/BookRepository.kt
+++ src/main/kotlin/repository/BookRepository.kt
@@ -1,12 +1,23 @@
 package repository
 
-import model.Book
-import model.Repository
-import model.Reservation
-import model.User
+import model.*
 
 class BookRepository: Repository<Book>() {
 
-    fun availableBooks(reservation: Reservation): MutableList<Book> = this.repositoryObjects().filter { it.isAvailable(reservation) }.toMutableList()
+    fun availableBooks(criteria: BookSearchCriteria): MutableList<Book> {
+        val reservation = Reservation(
+            user = User(),
+            pickUpDate = criteria.from,
+            dropOffDate = criteria.to
+        )
+        return this.repositoryObjects()
+            .filter { it.isAvailable(reservation) }
+            .filter { criteria.gender == null || it.gender == criteria.gender }
+            .filter { criteria.ISBN == null || it.ISBN == criteria.ISBN }
+            .filter { criteria.ownBy == null || it.owner == criteria.ownBy}
+//            .filter { criteria.pagesRangeFrom == null && criteria.pagesRangeTo == null || (it.numPages > criteria.pagesRangeFrom!! &&  it.numPages == criteria.pagesRangeTo!!) }
+            .filter { criteria.title == null || it.title.contains(criteria.title, ignoreCase = true) }
+            .toMutableList()
+    }
 
 }
