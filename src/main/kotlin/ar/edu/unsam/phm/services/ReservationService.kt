4862f252ceeeaa5302b328bac966999d7396d74e
nicky <nicolas_cernadas@hotmail.com>
2026-03-21 11:25:54 -0300

    arreglo

--- src/main/kotlin/ar/edu/unsam/phm/services/ReservationService.kt
+++ src/main/kotlin/ar/edu/unsam/phm/services/ReservationService.kt
@@ -138,6 +138,12 @@ class ReservationService(
         }
     }
 
+/*
+*
+* Todo por la interfaz de paginado
+*
+* */
+
     private fun sortByAscTitle(list: List<ReservationProfileDTO>): List<ReservationProfileDTO> =
         list.sortedBy {it.book.title}
 
