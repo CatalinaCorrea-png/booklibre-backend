ffabfe5a78b20c7cfcdd180fe70fc4ee7c77dd99
nicki <nicolas_cernadas@hotmail.com>
2026-03-26 18:27:16 -0300

    Mini fix

--- src/main/kotlin/ar/edu/unsam/phm/services/ReservationService.kt
+++ src/main/kotlin/ar/edu/unsam/phm/services/ReservationService.kt
@@ -111,11 +111,9 @@ class ReservationService(
 
     fun getUserOwnBooks(userId: Int): List<Reservation> {
         val everyUserOwnBook: List<Book> = bookRepository.findAllByUserId(userId)
-        val existingBookIds = everyUserOwnBook.map { it.id }.toSet()
 
         val reservationsWithBooksOwnByUser: List<Reservation> = reservationRepository
             .findByOwnerId(userId)
-            .filter { existingBookIds.contains(it.book.id) }
 
         val userNotReservedBooks = this.filterNoReservedBooks(everyUserOwnBook, reservationsWithBooksOwnByUser)
         val userNotReservedBooksInReservation = generateEmptyReservationsForNotReservedBooks(userNotReservedBooks)
@@ -126,23 +124,6 @@ class ReservationService(
 
         return everyUserBookInReservation
     }
-
-//    fun getUserOwnBooks(userId: Int): List<Reservation> {
-//
-//        val everyUserOwnBook: List<Book> = bookRepository.findAllByUserId(userId)
-//
-//        val reservationsWithBooksOwnByUser: List<Reservation> = reservationRepository.findByOwnerId(userId)
-//
-//        val userNotReservedBooks = this.filterNoReservedBooks(everyUserOwnBook, reservationsWithBooksOwnByUser)
-//
-//        val userNotReservedBooksInReservation = generateEmptyReservationsForNotReservedBooks(userNotReservedBooks)
-//
-//        val everyUserBookInReservation = (reservationsWithBooksOwnByUser + userNotReservedBooksInReservation)
-//            .sortedByDescending { it.pickUpDate }
-//            .distinctBy { it.book.id }
-//
-//        return everyUserBookInReservation
-//    }
 
     fun filterAndSortUserBooks(bookList: List<ReservationProfileDTO>, page: Int, pageSize: Int, filterCrit: FilterCriteria, sortCrit: SortCriteria): PagedResult<ReservationProfileDTO> {
         var filteredAndSortedBookList: List<ReservationProfileDTO> = bookList
