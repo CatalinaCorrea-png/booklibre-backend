4a0e0a71e4d9a1fa479405d73adb4193e84d517b
fernandatperez <fernanda.tperez@outlook.com>
2026-06-12 00:36:34 -0300

    fix in bootstrap, set existing books as not deleted

--- src/main/kotlin/ar/edu/unsam/phm/bootstrap/ProjectBootstrap.kt
+++ src/main/kotlin/ar/edu/unsam/phm/bootstrap/ProjectBootstrap.kt
@@ -255,6 +255,7 @@ class ProjectBootstrap : InitializingBean {
             // del Home no tenía datos y refillFromMongo traía un top arbitrario.
             existing.bookClicks = book.bookClicks
             existing.owner = book.owner
+            existing.deleted = false
             repoBooks.save(existing)
         } else {
             repoBooks.save(book)
